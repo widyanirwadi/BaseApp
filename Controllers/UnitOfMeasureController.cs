7aacc9e6c9b673edd59400a922e2b2eaf182d7de
WIDYAN IRWADI <015021@npid.com>
2023-06-08 08:30:29 +0700

    Remove unnecessary code

--- Controllers/UnitOfMeasureController.cs
+++ Controllers/UnitOfMeasureController.cs
@@ -2,8 +2,6 @@
 using BaseApp.Models;
 using BaseApp.Context;
 using Microsoft.AspNetCore.Mvc;
-/*using Microsoft.EntityFrameworkCore;*/
-using Microsoft.AspNetCore.SignalR;
 
 namespace BaseApp.Controllers
 {
@@ -11,40 +9,6 @@ namespace BaseApp.Controllers
     [Route("UnitOfMeasure")]
     public class UnitOfMeasureController : Controller
     {
-        /*
-        private readonly string connectionString = "Data Source=JKTITNB07\\SQLEXPRESS;Initial Catalog=AdventureWorks2022;Integrated Security=True; Encrypt=False";
-
-        public IActionResult Index()
-        {
-            List<UnitOfMeasure> unitOfMeasures = new List<UnitOfMeasure>();
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-
-                using (SqlCommand command = new SqlCommand("SELECT UnitMeasureCode, Name, ModifiedDate FROM Production.UnitMeasure", connection))
-                {
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            UnitOfMeasure unitOfMeasure = new UnitOfMeasure
-                            {
-                                UnitMeasureCode = reader.GetString(0),
-                                Name = reader.GetString(1),
-                                ModifiedDate = reader.GetDateTime(2)
-                            };
-
-                            unitOfMeasures.Add(unitOfMeasure);
-                        }
-                    }
-                }
-            }
-
-            return View(unitOfMeasures);
-        }
-        */
-
         private readonly IUnitOfMeasureRepository _UnitOfMeasureRepository;
         private readonly DBContext _dbContext;
         public UnitOfMeasureController(IUnitOfMeasureRepository UnitOfMeasureRepository, DBContext dbContext)
